2e30080d1c9244e44c5ddc81c48559b32b347f1e
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-03-11 21:54:31 +0800

    remove manually set authentication

--- src/main/java/com/projects/shortify_backend/security/CustomAuthenticationProvider.java
+++ src/main/java/com/projects/shortify_backend/security/CustomAuthenticationProvider.java
@@ -1,6 +1,5 @@
 package com.projects.shortify_backend.security;
 
-import com.projects.shortify_backend.services.UrlService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,7 +7,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -20,7 +18,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
-    private final UrlService urlService;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -37,10 +34,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
             throw new BadCredentialsException("Wrong password");
 
         var authenticatedUser = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-
-        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
-
-        log.info("Dashboard : {}", urlService.dashboard());
 
         log.info("Authentication : {}",authenticatedUser);
 
